c5878150026633b703271ec7117365cc7c0d663f
ravihoro <ravijohnhoro5@gmail.com>
2025-09-02 22:35:16 +0530

    Removed add event

--- app/src/main/java/com/example/filedownloader/presentation/event/DownloadEvent.kt
+++ app/src/main/java/com/example/filedownloader/presentation/event/DownloadEvent.kt
@@ -3,11 +3,8 @@ package com.example.filedownloader.presentation.event
 import com.example.filedownloader.data.local.DownloadTask
 
 sealed class DownloadEvent {
-
-    data class Add(val url: String): DownloadEvent()
     data class Start(val task: DownloadTask): DownloadEvent()
     data class Pause(val taskId: Int, val downloadedBytes: Long, val progress: Float) : DownloadEvent()
     data class Resume(val task: DownloadTask): DownloadEvent()
     data class Cancel(val taskId: Int): DownloadEvent()
-
 }
