d128d08e5bdc2fce02e9ad36a5d8e24bd5d130fa
ravihoro <ravijohnhoro5@gmail.com>
2025-09-02 17:36:30 +0530

    Code moved to file repository

--- app/src/main/java/com/example/filedownloader/utils/HelperFuntions.kt
+++ app/src/main/java/com/example/filedownloader/utils/HelperFuntions.kt
@@ -1,42 +1,7 @@
 package com.example.filedownloader.utils
 
-
-import android.content.ContentValues
-import android.content.Context
-import android.os.Build
-import android.os.Environment
-import android.provider.MediaStore
-import androidx.annotation.RequiresApi
-import java.io.File
-import java.io.FileInputStream
 import kotlin.math.floor
 import kotlin.text.*
-
-@RequiresApi(Build.VERSION_CODES.Q)
-fun saveFileToDownloads(context: Context, file: File, mimeType: String) {
-    val resolver = context.contentResolver;
-
-    val contentValues = ContentValues().apply {
-        put(MediaStore.Downloads.DISPLAY_NAME, file.name)
-        put(MediaStore.Downloads.MIME_TYPE, mimeType)
-        put(MediaStore.Downloads.RELATIVE_PATH, Environment.DIRECTORY_DOWNLOADS)
-    }
-
-    val uri = resolver.insert(MediaStore.Downloads.EXTERNAL_CONTENT_URI, contentValues)
-    if(uri != null){
-        try {
-            resolver.openOutputStream(uri).use { outputStream ->
-                FileInputStream(file).use { inputStream ->
-                    inputStream.copyTo(outputStream!!)
-                }
-            }
-        } catch (e: Exception) {
-            throw Exception("Failed to save the file: ${e.message}", e)
-        }
-    }else{
-        throw Exception("Uri is nullll")
-    }
-}
 
 
 fun formatBytes(bytes: Long): String {
