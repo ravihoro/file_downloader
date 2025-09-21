498b051573ee39052c2f455827535942b62813cc
ravihoro <ravijohnhoro5@gmail.com>
2025-09-21 13:57:50 +0530

    Removed work manager and added inappdownloadmanager

--- app/src/main/java/com/example/filedownloader/FileDownloaderApp.kt
+++ app/src/main/java/com/example/filedownloader/FileDownloaderApp.kt
@@ -1,19 +1,28 @@
 package com.example.filedownloader
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
-import androidx.work.Configuration
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.example.filedownloader.data.manager.InAppDownloadManager
+import com.example.filedownloader.data.notification.DownloadNotificationManager
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltAndroidApp
-class FileDownloaderApp: Application(), Configuration.Provider {
+class FileDownloaderApp: Application() {
 
-    @Inject
-    lateinit var workerFactory: HiltWorkerFactory
+    @Inject lateinit var inAppDownloadManager: InAppDownloadManager
+    @Inject lateinit var notificationManager: DownloadNotificationManager
 
-    override val workManagerConfiguration: Configuration
-        get() = Configuration.Builder()
-            .setWorkerFactory(workerFactory)
-            .build()
+    @RequiresApi(Build.VERSION_CODES.Q)
+    override fun onCreate() {
+        super.onCreate()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            inAppDownloadManager.reconcileOnStartup(resumeActiveToAuto = false)
+        }
+    }
 }
