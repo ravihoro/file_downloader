8708a361fab12eff53ba3d444a4dcde43ae4daef
ravihoro <ravijohnhoro5@gmail.com>
2025-09-21 13:56:54 +0530

    Added pause download

--- app/src/main/java/com/example/filedownloader/data/orchestrator/DownloadOrchestrator.kt
+++ app/src/main/java/com/example/filedownloader/data/orchestrator/DownloadOrchestrator.kt
@@ -3,6 +3,7 @@ package com.example.filedownloader.data.orchestrator
 interface DownloadOrchestrator {
 
     fun enqueueDownload(taskId: Int)
+    fun pauseDownload(taskId: Int)
     fun cancelDownload(taskId: Int)
     fun resumeDownload(taskId: Int)
     suspend fun startNextDownload()
